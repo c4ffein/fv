7c1ce90cb38fb7fbb6d105d5ca0a62066d621afb
c4ffein <c4ffein@gmail.com>
2024-11-04 22:00:24 +0100

    linted test

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-from os import mkdir, rmdir, remove, listdir
-from shutil import rmtree
-from unittest import TestCase, main as unittest_main, mock
-from json import loads, dumps
-from pathlib import Path
 from hashlib import sha256
+from json import dumps, loads
+from os import listdir, remove
+from pathlib import Path
+from shutil import rmtree
+from unittest import TestCase, mock
+from unittest import main as unittest_main
 
-from fv import sha256sum, encrypt_file, decrypt_file, get_index, acquire_lock, release_lock, store_file, retrieve_file
+from fv import acquire_lock, decrypt_file, encrypt_file, get_index, release_lock, retrieve_file, sha256sum, store_file
 
 TESTING_DIR = "fv_testing_dir_947f6128-4d75-40bb-a9ad-1a64342bd860"
 STORE_DIR = f"{TESTING_DIR}/store_dir"  # TODO?
@@ -20,7 +21,7 @@ class FVTest(TestCase):
 
     def setUp(self):
         self._clean_test_dir()
-        mkdir(TESTING_DIR)
+        Path(TESTING_DIR).mkdir()
 
     def tearDown(self):
         self._clean_test_dir()
@@ -30,7 +31,7 @@ class FVTest(TestCase):
             f.write("I am a goofy file")
         self.assertEqual(
             sha256sum(f"{TESTING_DIR}/test_mdr1.txt"),
-            "076dd4d0add0c2ce14af7579551fea579f7671ee1668421c12f080b5f25af1d2"
+            "076dd4d0add0c2ce14af7579551fea579f7671ee1668421c12f080b5f25af1d2",
         )
 
     def test_encrypt_decrypt(self):
@@ -40,7 +41,7 @@ class FVTest(TestCase):
         encrypt_file(f"{TESTING_DIR}/test_mdr1.txt", "stoopid-Password-0")
         remove(f"{TESTING_DIR}/test_mdr1.txt")
         decrypt_file(f"{TESTING_DIR}/test_mdr1.txt.gpg", "stoopid-Password-0")
-        with open(f"{TESTING_DIR}/test_mdr1.txt", "r") as f:
+        with open(f"{TESTING_DIR}/test_mdr1.txt") as f:
             o = f.read()
         assert o == "I am a goofy file"
 
@@ -48,10 +49,10 @@ class FVTest(TestCase):
         with open(f"{TESTING_DIR}/test_mdr1.txt", "w") as f:
             f.write("I am a goofy file")
         with self.assertRaises(Exception):
-            encrypt_file(f"{TESTING_DIR}/test_mdr1.txt", "stoopid-password-\"")
+            encrypt_file(f"{TESTING_DIR}/test_mdr1.txt", 'stoopid-password-"')
 
     def test_can_get_hex_index(self):
-        mkdir(f"{TESTING_DIR}/index")
+        Path(f"{TESTING_DIR}/index").mkdir()
         with open(f"{TESTING_DIR}/index/00a200030004000a.json", "w") as f:
             f.write(dumps({"I am": "an index"}))
         self.assertEqual(get_index(f"{TESTING_DIR}"), (45598959112290314, {"I am": "an index"}))
@@ -71,12 +72,12 @@ class FVTest(TestCase):
         acquire_lock(f"{TESTING_DIR}/store")
         with self.assertRaises(Exception):
             store_file(f"{TESTING_DIR}/store", f"{TESTING_DIR}/file.txt", "pass")
-    
+
     def test_cant_retrieve_while_locked(self):
         acquire_lock(f"{TESTING_DIR}/store")
         with self.assertRaises(Exception):
             retrieve_file(f"{TESTING_DIR}/store", "42")
-    
+
     def _load_a_file(self, file_name, file_content):
         with open(f"{TESTING_DIR}/{file_name}", "w") as f:
             f.write(file_content)
@@ -85,7 +86,7 @@ class FVTest(TestCase):
 
     def test_load_1_file(self):
         file_1_sum = self._load_a_file("file.txt", "I am a goofy file")
-        with open(f"{TESTING_DIR}/store/index/0000000000000001.json", "r") as f:
+        with open(f"{TESTING_DIR}/store/index/0000000000000001.json") as f:
             index = loads(f.read())
         self.assertEqual(type(index), dict)
         self.assertEqual([(len(k), len(v)) for k, v in index.items()], [(36, 4)])
@@ -100,12 +101,12 @@ class FVTest(TestCase):
     def test_load_2_files(self):
         file_1_sum = self._load_a_file("file_1.txt", "I am a first goofy file")
         file_2_sum = self._load_a_file("file_2.txt", "I am a second goofy file")
-        with open(f"{TESTING_DIR}/store/index/0000000000000002.json", "r") as f:
+        with open(f"{TESTING_DIR}/store/index/0000000000000002.json") as f:
             index = loads(f.read())
         self.assertEqual(type(index), dict)
         self.assertEqual(len(index), 2)
         self.assertEqual([(len(k), len(v)) for k, v in index.items()], [(36, 4), (36, 4)])
-        values = [v for v in index.values()]
+        values = list(index.values())
         self.assertEqual((values[0][1], values[0][3]), (file_1_sum, "file_1.txt"))
         self.assertEqual((values[1][1], values[1][3]), (file_2_sum, "file_2.txt"))
         index_1, index_2 = index.keys()
@@ -134,17 +135,15 @@ class FVTest(TestCase):
         retrieve_file(f"{TESTING_DIR}/store", files[0])
         retrieve_file(f"{TESTING_DIR}/store", files[1])
         self.assertEqual(len(listdir(Path(f"{TESTING_DIR}/store/files"))), 2)
-        the_set = {
-            sha256sum(f"{TESTING_DIR}/store/files/{files[0]}"), sha256sum(f"{TESTING_DIR}/store/files/{files[1]}")
-        }
+        the_set = {sha256sum(f"{TESTING_DIR}/store/files/{files[i]}") for i in (0, 1)}
         self.assertEqual(the_set, {file_1_sum, file_2_sum})
-    
+
     def test_load_and_retrieve_a_non_cached_file(self):
         file_1_sum = self._load_a_file("file.txt", "I am a goofy file")
         files = listdir(Path(f"{TESTING_DIR}/store/files"))
         self.assertEqual(len(files), 1)
         rmtree(f"{TESTING_DIR}/store/files")
-        mkdir(f"{TESTING_DIR}/store/files")
+        Path(f"{TESTING_DIR}/store/files").mkdir()
         self.assertEqual(len(listdir(Path(f"{TESTING_DIR}/store/files"))), 0)
         retrieve_file(f"{TESTING_DIR}/store", files[0])
         self.assertEqual(len(listdir(Path(f"{TESTING_DIR}/store/files"))), 1)
@@ -156,14 +155,12 @@ class FVTest(TestCase):
         files = listdir(Path(f"{TESTING_DIR}/store/files"))
         self.assertEqual(len(files), 2)
         rmtree(f"{TESTING_DIR}/store/files")
-        mkdir(f"{TESTING_DIR}/store/files")
+        Path(f"{TESTING_DIR}/store/files").mkdir()
         self.assertEqual(len(listdir(Path(f"{TESTING_DIR}/store/files"))), 0)
         retrieve_file(f"{TESTING_DIR}/store", files[0])
         retrieve_file(f"{TESTING_DIR}/store", files[1])
         self.assertEqual(len(listdir(Path(f"{TESTING_DIR}/store/files"))), 2)
-        the_set = {
-            sha256sum(f"{TESTING_DIR}/store/files/{files[0]}"), sha256sum(f"{TESTING_DIR}/store/files/{files[1]}")
-        }
+        the_set = {sha256sum(f"{TESTING_DIR}/store/files/{files[i]}") for i in (0, 1)}
         self.assertEqual(the_set, {file_1_sum, file_2_sum})
 
 
