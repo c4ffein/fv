7b56e99d61d96e4ef605d3c89a13b5e61f0dd519
c4ffein <c4ffein@gmail.com>
2025-10-18 23:54:13 +0200

    just prettier help

--- fv.py
+++ fv.py
@@ -155,14 +155,14 @@ def usage(wrong_config=False, wrong_command=False, wrong_arg_len=False):
     conf = """~/.config/fv/init.json => {"stores": {"default": {"path": "path-that-will-include-the-subdirs"}}}"""
     output_lines = [
         "fv - File Vault",
-        "===============",
+        "───────────────",
         conf,
         "  - creates 4 subdirs:\n    - files\n    - encrypted_files\n    - index\n    - wip",
-        "===============",
+        "───────────────",
         "- fv i file_path         ==> encrypt with a single-use password, index, and store a file in /encrypted_files",
         "- fv o uuid              ==> recover an indexed file from /encrypted_files to /file using the uuid from i",
         "- fv [[path] OR [uuid]]  ==> retrieves if the argument is an uuid, else stores as path",
-        "===============",
+        "───────────────",
         "You can store any file and record its uuid in your knowledge base or any other external tool",
         "You can version /indexes and securely share it between your local devices",
         "You can remote sync /encrypted_files to many remote unsecure servers as those are encrypted and hashed",
